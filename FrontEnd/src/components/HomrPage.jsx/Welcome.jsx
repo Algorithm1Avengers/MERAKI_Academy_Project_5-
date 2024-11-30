f2f933006c2392a87af39491aa13684c2522eb1f
Haneen abulebbeh <haneenabulebbeh@gmail.com>
2024-11-30 18:47:42 +0300

    handling search

--- FrontEnd/src/components/HomrPage.jsx/Welcome.jsx
+++ FrontEnd/src/components/HomrPage.jsx/Welcome.jsx
@@ -1,27 +1,58 @@
-import React from 'react'
+import {React, useState} from 'react'
 import Carousel from 'react-bootstrap/Carousel';
+import { useNavigate } from 'react-router-dom';
 import "./style.css"
 // import {Input} from '@mui/joy/Input';
 import Box from "@mui/material/Box";
-
+import axios from 'axios';
 
 const Welcome = ()=> {
+  const [searchInput, setSearchInput] = useState("");
+  const [spotId, setSpotId]=useState()
+  const navigate = useNavigate();
+  const handleSearch = async () => {
+ 
+
+  
+    try {
+      const result =  await axios.get(`http://localhost:5000/touristspot/name/${searchInput}`); 
+     
+   
+    
+      if (result?.data?.success) {
+
+        console.log("Search results:", result.data.result);
+        setSearchInput(result.data.result);
+        setSpotId(result.data.result.category_id)
+        console.log(spotId);
+        
+        navigate(`/TouristSpots/${spotId}/TouristSpots-Detailes/${searchInput}`)
+       
+      } else {
+        console.error("Search error:", data.message);
+        setSearchInput("Search not Found")
+      }
+    } catch (error) {
+      console.error("Search failed:", error);
+      
+    }
+  };
+  
   return (
     <>
     {/* Search box*/}
     <div className="header-content">
-          <h1>Where do you want to travel?</h1>
+          <h1>Where do you want to travel?<br/>
+          <span className='text'>
+            Start Your Virtual Tour</span></h1>
           <div className="search-bar">
               <input id="searchInput" placeholder="Search for a place..." onChange={(e)=>{setSearchInput(e.target.value)}}/>
-              <button id="searchButton">Search</button>
+              <button id="searchButton" onClick={() => handleSearch()}>Search</button>
           </div>
     </div>
 
     
-   {/* <video autoPlay muted loop className="header-background" style={{ width: "60rem", height: "20rem", alignItems:"center"}}>
-          <source src="./src/assets/video.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video> */}
+  
 
         
   <Carousel className='carousel' fade   style={{ width: '100%',marginBottom:"2px",}} >
@@ -78,6 +109,8 @@ const Welcome = ()=> {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+    {/* <div>{searchInput}</div> */}
+    
     </>
   )
 }
